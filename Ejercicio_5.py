3dc645d6ce7fcd79313efe999fbadd0aec98f0d4
andrea1807 <andreamelenje1807@gmail.co>
2020-08-21 12:41:31 -0500

    commit5
    ej5

---
+++ Ejercicio_5.py
@@ -0,0 +1,4 @@
+num1=int(input("numero1: "))
+num2=int(input("numero2: "))
+for n in range(num1,num2):
+    print(n)
---
+++ Ejercicio_6.py
@@ -0,0 +1,4 @@
+for i in range(1,11):
+    print(f"Tablas de multiplicacion {i} : ")
+    for j in range(1,11):   
+     print(str(i)+" * "+str(j)+ " = "+  format(i*j))
