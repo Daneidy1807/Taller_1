2ace116e4a07ec6124108468cc2429db3c692c1c
andrea1807 <andreamelenje1807@gmail.co>
2020-08-21 12:41:17 -0500

    commit4
    ej4

---
+++ Ejercicio_4.py
@@ -0,0 +1,10 @@
+numero1=int(input("numero1: "))
+numero2=int(input("numero2: "))
+suma=numero1+numero2
+resta=numero1-numero2
+multiplicacion=numero1*numero2
+division=numero1/numero2
+print(f"la Suma es: {suma}")
+print(f"la esta es: {resta}")
+print(f"la multiplicacion es: {multiplicacion}")
+print(f"la division es {division}")
